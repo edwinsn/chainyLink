981a4e0e5382a36ec38e7bed4c7a0b808b8c7598
edwinsn <edwin@sprwt.io>
2022-11-07 00:21:42 -0500

    Route for the links page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './Assets/css/layout.css';
 import './Assets/css/tags.css';
 import './Assets/css/colors.css';
 import './Assets/css/sizes.css';
+import './Assets/css/links.css'
 import Routes from './Routes';
 
 
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,16 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Home from './Home';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import About from './Home';
 import Login from './Login';
 import Register from './Register';
+import GenerateLink from './GenerateLink';
 
 export default function App() {
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path='/sign-up' element={<Register />} />
-            {/*<Route path="/links" element={<Links />} />*/}
-            {/*<Route path="/links/:id" element={<Link />} />*/}
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<GenerateLink />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path='/sign-up' element={<Register />} />
+                {/*<Route path="/links" element={<Links />} />*/}
+                {/*<Route path="/links/:id" element={<Link />} />*/}
+            </Routes>
+        </BrowserRouter>
     );
 }
