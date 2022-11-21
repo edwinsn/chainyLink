d2df8dc46e7da7799a0a36e89c00389f3b488be4
edwinsn <edwin@sprwt.io>
2022-11-20 23:43:32 -0500

    Slice for the links background color

--- src/reducers/features/links.js
+++ src/reducers/features/links.js
@@ -2,17 +2,20 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const cardsSlice = createSlice({
     name: 'links',
-    initialState: { numberOfLinks: 2 },
+    initialState: { numberOfLinks: 2, backgroundColor: '#EDF2FB' },
     reducers: {
         addLink: (state) => {
             state.numberOfLinks = state.numberOfLinks < 100 ? state.numberOfLinks + 1 : 100
         },
         removeLink: (state) => {
             state.numberOfLinks = state.numberOfLinks > 0 ? state.numberOfLinks - 1 : 0
+        },
+        setBackgroundColor: (state, action) => {
+            state.backgroundColor = action.payload
         }
     },
 })
 
-export const { addLink, removeLink } = cardsSlice.actions
+export const { addLink, removeLink, setBackgroundColor } = cardsSlice.actions
 
 export default cardsSlice.reducer
