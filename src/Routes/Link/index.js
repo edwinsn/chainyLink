65a200f31224826c1233313e8699eb805deb4108
edwinsn <edwin@sprwt.io>
2022-11-23 02:42:42 -0500

    Showed the link color in the link page

--- src/Routes/Link/index.js
+++ src/Routes/Link/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LinksList from './components/LinksList'
 //import ParentLink from './components/ParentLink'
 import useLink from './hooks/useLink'
 import './Assets/links.css'
+import { useDispatch } from 'react-redux'
+import { setBackgroundColor } from '../../reducers/features/links'
 
 export default function Link() {
 
     const id = window.location.pathname.split('/')[2]
     const [link] = useLink(id)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+        dispatch(setBackgroundColor(link?.background))
+
+    }, [link?.background, dispatch])
 
     return (
         <section>
