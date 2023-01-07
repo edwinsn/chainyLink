50cc204b308ff7395a5246a416c08b2ff9b5ba89
edwinsn <edwin@sprwt.io>
2023-01-07 00:28:49 -0500

    When no child link in a links group show a text saying it

--- src/Routes/Link/components/LinksList.js
+++ src/Routes/Link/components/LinksList.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import Link from './Link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@cseitz/icons-duotone';
 
 export default function LinksList({ links }) {
 
     return (
         <div className='centered flex-column'>
             {
-                links?.map((link, index) => (
+                links?.length ? links?.map((link, index) => (
                     <Link
                         content={link?.url}
                         id={`link-${index}`}
                         key={`link-${index}`}
-                        backgroundColor={link?.background}
+                        backgroundColor={link?.background||"#ffffff"}
                     />
-                ))
+                )) : (
+                    <p className='gray my-3 bg-gray px-2 py-1 circle-rounded'>
+                        <FontAwesomeIcon
+                            icon={faLink}
+                            className="mr-1" />
+                        No links
+                    </p>
+                )
             }
         </div>
     )
