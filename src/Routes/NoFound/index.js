c77101669eb5019a1a736578d10490240ad2c8cf
edwinsn <edwin@sprwt.io>
2023-02-09 01:41:41 -0500

    Updated the not found page icon

--- src/Routes/NoFound/index.js
+++ src/Routes/NoFound/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import brokenLinkIcon from '../../Assets/Images/broken-link.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkSlash } from '@cseitz/icons-duotone';
+
 
 export default function index() {
     return (
         <section className='flex centered'>
-            <a 
-            href='/'
-            className='circle-rounded bg-gray flex centered px-2 py-1 pointer no_decoration'>
-                <img
-                    src={brokenLinkIcon}
-                    alt="" 
-                    className='h-1 mr-1'
-                    />
+            <a
+                href='/'
+                className='circle-rounded bg-gray flex centered px-2 py-1 pointer no_decoration'>
+                <FontAwesomeIcon
+                    icon={faLinkSlash}
+                    className="mr-1" />
                 <span className='gray'>
                     Page not found
                 </span>
